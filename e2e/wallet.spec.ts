fb20938f44d84aa1319a06029ee75201ed810c0d
Nahuel Santoalla <nahjersan@gmail.com>
2025-08-02 18:34:17 +0200

    Add missing tests

--- e2e/wallet.spec.ts
+++ e2e/wallet.spec.ts
@@ -5,17 +5,45 @@ import walletSetup, { ADDRESS } from './wallet.setup'
 const it = testWithSynpress(metaMaskFixtures(walletSetup))
 const { expect, describe, beforeEach } = it
 
-describe('While connected', () => {
-  beforeEach(async ({ context, page, metamaskPage, extensionId }) => {
-    const metamask = new MetaMask(context, metamaskPage, walletSetup.walletPassword, extensionId)
-    await page.goto('/')
+const shortAddress = ADDRESS.slice(0, 6) + '...' + ADDRESS.slice(-4)
 
+let metamask: MetaMask
+beforeEach(async ({ context, metamaskPage, extensionId }) => {
+  metamask = new MetaMask(context, metamaskPage, walletSetup.walletPassword, extensionId)
+})
+
+it('should connect to wallet', async ({ page }) => {
+  await page.goto('/')
+  await expect(page.getByText('Connect')).toBeAttached()
+
+  await page.getByText('Connect').click()
+  await metamask.connectToDapp()
+
+  await expect(page.getByText(shortAddress)).toBeAttached()
+})
+
+describe('While connected to wallet', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('/')
     await page.getByText('Connect').click()
     await metamask.connectToDapp()
   })
 
-  it('should be connected', async ({ page }) => {
-    const shortAddress = ADDRESS.slice(0, 6) + '...' + ADDRESS.slice(-4)
+  it('should remember previous connections', async ({ page }) => {
     await expect(page.getByText(shortAddress)).toBeAttached()
+
+    await page.reload()
+
+    await expect(page.getByText(shortAddress)).toBeAttached()
+  })
+
+  it('should NOT remember when the user disconnects', async ({ page }) => {
+    await expect(page.getByText(shortAddress)).toBeAttached()
+
+    await metamask.lock()
+    await page.reload()
+
+    await expect(page.getByText(shortAddress)).not.toBeAttached()
+    await expect(page.getByText('Connect')).toBeAttached()
   })
 })
